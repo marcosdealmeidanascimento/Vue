6a3fcc8a0d959570e130365d08469230d4d17fbb
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2022-11-30 15:08:25 -0300

    Continuação Curso

--- code/app/src/App.vue
+++ code/app/src/App.vue
@@ -1,15 +1,40 @@
 <template>
+  <TheHeader v-if="showHeader"></TheHeader>
+
+
+
+  <div v-show="showName">
+    <p>Nome: {{ name }}</p>
+    <p>Sobronenome: {{ lastname }}</p>
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+  <div v-else>Usuário</div>
+
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      showName: true,
+      name: 'Gamer',
+      lastname: 'Pilk',
+      accessLevel: ''
+    }
   }
 }
 </script>
