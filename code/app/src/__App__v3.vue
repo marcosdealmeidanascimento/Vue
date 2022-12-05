109ddd8bf0a278c40f7ff38f912b767b53cd2090
marcosdealmeidanascimento <marcos.800nascimento@gmail.com>
2022-12-05 10:04:33 -0300

    Terceiro Exemplo de Vue

---
+++ code/app/src/__App__v3.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+        <button @click="onClick" type="submit">Botao</button>
+        <p>{{ fullName }}</p><br><br>
+        <h3>Todos</h3>
+        <div v-for="todo in todos"
+        :key="todo.id">
+            <input v-model="todo.completed" type="checkbox">
+                {{ todo.title }}
+        </div>
+        <br><br>
+        <p>Todos Em Aberto</p>
+        <div v-for="todo in uncompletedTodos" :key="todo.id">
+            {{ todo.title }}
+        </div>
+        <br><br>
+        <p>Todos Completas</p>
+        <div v-for="todo in completedTodos" :key="todo.id">
+            {{ todo.title }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data() {
+        return {
+            user: {
+                first_name: 'A',
+                last_name: 'B'
+            },
+
+            todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "et porro tempora",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                    "completed": false
+                }
+                ]
+        }
+    },
+
+    computed: {
+        fullName() {
+            return `${this.user.first_name} ${this.user.last_name}`  
+        },
+
+        completedTodos() {
+            return this.todos.filter(todo => todo.completed)
+        },
+        
+        uncompletedTodos() {
+            return this.todos.filter(todo => !todo.completed)
+        }
+    },
+
+    methods: {
+        onClick(){
+            console.log(this.fullName)
+        } 
+    }
+}
+</script>
+
+<style>
+*{
+    margin: 0px;
+    padding: 20px;
+}
+
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
